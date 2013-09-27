835fcc72e40e5beb5ec60cf54bb21d03114ba797
John Chapin <john.chapin@gmail.com>
2013-09-27 12:17:38 +0800

    Use clojure 1.5.1

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :min-lein-version "2.0.0"
-  :dependencies [[org.clojure/clojure "1.4.0"]
+  :dependencies [[org.clojure/clojure "1.5.1"]
                  [link "0.4.0"]
                  [info.sunng/carbonite "0.2.3"]
                  [cheshire "4.0.3"]
